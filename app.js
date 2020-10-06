93903b6c2e2bffefd635f95165f0d98b9c4b6d70
EthernalSlayer <guillaume.b.a@protonmail.com>
2020-10-06 09:21:09 +0200

    final solution to express tdd quest

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 // app.js
 const express = require("express");
 const bodyParser = require("body-parser");
+require("dotenv").config();
+const connection = require("./connection");
 
 const app = express();
 app.use(bodyParser.json());
@@ -8,6 +10,43 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Hello World!" });
+});
+
+app.get("/bookmarks/:id", (req, res) => {
+  connection.query(
+    "SELECT * FROM bookmark WHERE id = ?",
+    [req.params.id],
+    (err, results) => {
+      if (err || results.length === 0) {
+        res.status(404).json({ error: "Bookmark not found" });
+      } else {
+        res.status(200).json(results[0]);
+      }
+    }
+  );
+});
+
+app.post("/bookmarks", (req, res) => {
+  if (!req.body.url || !req.body.title) {
+    res.status(422).json({ error: "required field(s) missing" });
+  } else {
+    connection.query(
+      "INSERT INTO bookmark SET ?",
+      [req.body],
+      (err, results) => {
+        if (err) {
+          res.status(500).send("Error...");
+        } else {
+          connection.query(
+            `SELECT * FROM bookmark WHERE id = ${results.insertId}`,
+            (err, results) => {
+              res.status(201).json(results[0]);
+            }
+          );
+        }
+      }
+    );
+  }
 });
 
 module.exports = app;
